d4d9570d58cb428a43a01e14592623eebd465c13
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-05-23 20:51:41 -0400

    completed!

--- main.rb
+++ main.rb
@@ -14,32 +14,67 @@ class Words
 end
 class Game
     attr_reader :words, :word, :solution
-    attr_accessor :guess
+    attr_accessor :guess, :correct, :blanks
     def initialize
         @words = Words.new
         @word = @words.word_gen
         @solution = @word.split("")
-        @blanks = blanks_gen(@word, @guess)
+        @correct = []
         @guess = []
-        @incorrect = []
+        @blanks = blanks_gen(@word, @correct)
+        @incorrect = 0
+        @winner = false
     end
-    def blanks_gen(wordle, guess)
+    def blanks_gen(base_word, right)
         arr = []
-        word_length = wordle.split("")
+        word_arr = base_word.split("")
         x = 0
-        until x == word_length.length do
-            arr.push("_")
-            x += 1
+        until x == word_arr.length do
+            if right.length > 0
+                word_arr.each do |a|
+                    if right.include?(a)
+                        arr.push(a)
+                    else
+                        arr.push("_")
+                    end
+                    x += 1
+                end
+            else
+                arr.push("_")
+                x += 1
+            end          
         end
         arr.join(" ")
+    end
+    def check_guess(check)
+        if @solution.include?(check)
+            return true
+        else
+            return false
+        end
+    end
+    def check_win
+        check = []
+        @solution.each do |a|
+            if @correct.include?(a)
+                check.push(true)
+            else
+                check.push(false)
+            end
+        end
+        if check.include?(false) || check.length == 0
+            return false
+        else
+            return true
+        end
     end
     def view_word
         puts @word
     end
     def game_play
-        x = 0
-        until x == 5
+        until @incorrect == 5
             puts "Guess the word! You can only make 5 mistakes!"
+            puts @blanks
             alphabet = "abcdefghijklmnopqrstuvwxyz"
             arr = alphabet.split('')
             input = gets.chomp
@@ -49,10 +84,26 @@ class Game
                         input = gets.chomp
                     end
                 end
-                puts "you picked: #{input}!"
+                puts "you picked: #{input.downcase}!"
                 @guess.push(input.downcase)
                 puts "Your guesses: #{@guess.join(" - ")}"
-                
+                if check_guess(input.downcase) == true
+                    @correct.push(input.downcase)
+                    if check_win == true
+                        puts "You win!"
+                        puts "The word was #{@word}!"
+                        break
+                    end
+                else
+                    @blanks = blanks_gen(@word, @correct)
+                    @incorrect += 1
+                    puts "Incorrect! You've made #{@incorrect}/5 incorrect choices! "
+                end
+                @blanks = blanks_gen(@word, @correct)    
+        end
+        if check_win == false
+            puts "Too bad, you lose!"
+            puts "The word was #{@word}!"
         end
     end
 end
